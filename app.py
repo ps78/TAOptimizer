de83c416be7f4f7e1669052d900f045c07be33e7
Pascal Simon <mail@pascalsimon.com>
2023-03-05 14:02:01 +0100

    added parallel processing

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import numpy as np
 import time
-import multiprocessing
+from multiprocessing import Process, Manager
 
 from searching import DfsSearch, SearchResult, SolutionItem
 from game import BaseLayout, Constants
@@ -66,8 +66,6 @@ def best_power_layout_from_image(img_in :str, n_total_buildings :int=38, top_n :
         next_level_threshold:  if this is set to a value >0, the search is started with top_n=1 and then increased
                            to top_n if the power rate was bigger than this threshold
     """
-    start_time = time.time()    
-
     # scan the given image    
     ib = Image2Base()
     layouts = ib.find_layouts(img_in)
@@ -83,40 +81,51 @@ def best_power_layout_from_image(img_in :str, n_total_buildings :int=38, top_n :
         top_n_next :int = top_n
         print(f"Searching for best layout with top_n={top_n}")
     
-    print_result = lambda idx, result: print(f"  Layout {idx:>2}: {result.best.power_rate:,}/h {result.best.num_accus:>2} accus {result.iterations:>5,} iterations {result.runtime:>8.3f} sec")
-    print_stats = lambda n_layouts, runtime: print(f"Processed {n_layouts} layouts in {runtime:.1f} seconds ({runtime/n_layouts:.3f} sec/layout)")
+    print_result = lambda result: print(f"  Layout {result.layout_index:>2}: "
+                                        f"{result.search_result.best.power_rate:>15,}/h "
+                                        f"{result.search_result.best.num_accus:>5} accus "
+                                        f"{result.search_result.iterations:>8,} iterations "
+                                        f"{result.search_result.runtime:>10.3f} sec")
+    print_stats = lambda n_layouts, runtime: print(f"Processed {n_layouts} layouts in {runtime:.1f} seconds "
+                                                    f"({runtime/n_layouts:.3f} sec/layout)")
 
     # we store all search reasults in a list of tuples : (layout-index, layout, search result)
     results :list[LayoutSearchResult] = []
 
-    # first pass of search with top_n=top_n_start    
-    for idx, layout in enumerate(layouts):
-        result :SearchResult = _process_layout(layout, top_n_start, n_total_buildings)
-        print_result(idx, result)
-        results.append(LayoutSearchResult(idx, layout, result))
-    n_first_pass_results = len(results)
-    print_stats(n_first_pass_results, time.time()-start_time)
+    for topn in range(top_n_start, top_n_next+1):
+        start_time = time.time()
+        print(f"Searching with top_n={topn}")
 
-    # search again with a larger top_n, if requested
-    start_time = time.time()
-    if top_n_next > top_n_start:
-        print(f"Repeat search with top_n={top_n_next}")
-        for result_idx in range(n_first_pass_results):
-            if results[result_idx].search_result.best.power_rate > next_level_threshold:                
-                result :SearchResult = _process_layout(layout, top_n_next, n_total_buildings)
-                results.append(LayoutSearchResult(idx, layout, result))
-                print_result(idx, result)
-        print_stats(len(results)-n_first_pass_results, time.time()-start_time)
-    
+        results_idx_start = len(results)
+        with Manager() as manager:
+            process_results :dict[int,LayoutSearchResult] = manager.dict()
+            jobs = []
+            for idx, layout in enumerate(layouts):
+                process_results[idx] = None
+                if results_idx_start==0 or results[idx].search_result.best.power_rate > next_level_threshold:                    
+                    process = Process(target=_process_layout, args=(idx, layout, topn, n_total_buildings, process_results))
+                    jobs.append(process)
+            
+            if len(jobs) > 0:
+                [j.start() for j in jobs]
+                [j.join() for j in jobs]
+                for _, result in process_results.items():
+                    results.append(result)
+                    print_result(result)
+                num_results = len(results) - results_idx_start
+                print_stats(num_results, time.time()-start_time)
+
     # show stats
     results.sort(reverse=True, key=lambda x: x.search_result.best.power_rate)
-    print(f"Best layout is {results[0].layout_index} and has a power production rate of { results[0].search_result.best.power_rate:,}/h")
+    top = results[0]
+    print(f"Best layout is {top.layout_index} and has a power production rate of { top.search_result.best.power_rate:,}/h")
 
     # write output image
     p = Path(img_in)
     pars = f"_b{n_total_buildings}_topn{top_n}"
     img_out = f"{Path.joinpath(p.parent, p.stem + pars + p.suffix)}"
-    ib.write_rates_to_image(img_in, img_out, [CoordinateRateTuple(r.layout_index, r.layout.coord, r.search_result.best.power_rate) for r in results])
+    data :list[CoordinateRateTuple] = [CoordinateRateTuple(r.layout_index, r.layout.coord, r.search_result.best.power_rate) for r in results]
+    ib.write_rates_to_image(img_in, img_out, data)
     print(f"Results written to {img_out}")
 
     # generate CncTAOpt-link to best layout
@@ -126,8 +135,11 @@ def best_power_layout_from_image(img_in :str, n_total_buildings :int=38, top_n :
     cnctaopt = CncTaOptParser(field = lay.field)
     print(f"CncTAOpt-Url to best layout:\n{cnctaopt.generate_url()}\n\n")
 
-def _process_layout(layout: ImageLayout, top_n :int, n_total_buildings :int) -> SearchResult:
+def _process_layout(layout_index :int, layout: ImageLayout, top_n :int, n_total_buildings :int, results_output :dict[int,LayoutSearchResult]):
+    """
+    Searches the best setup for the given layout using the given parameters
+    This is called by function best_power_layout_from_image()
+    """
     search = DfsSearch(BaseLayout(layout.data))        
-    result :SearchResult = search.find_best_power(top_n=top_n, n_total_buildings=n_total_buildings)
-    return result
-    
+    search_result :SearchResult = search.find_best_power(top_n=top_n, n_total_buildings=n_total_buildings)
+    results_output[layout_index] = LayoutSearchResult(layout_index, layout, search_result)
--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 from app import best_power_layout_from_url, best_power_layout_from_image
 from image2base import Image2Base
 
-#best_power_layout_from_url("https://cnctaopt.com/EPWsn", n_total_buildings=38, top_n=2) # 14.03
-#best_power_layout_from_url("https://cnctaopt.com/3yrxR", n_total_buildings=38, top_n=2) # 14.07
+if __name__ == "__main__":
+    #best_power_layout_from_url("https://cnctaopt.com/EPWsn", n_total_buildings=38, top_n=2) # 14.03
+    #best_power_layout_from_url("https://cnctaopt.com/3yrxR", n_total_buildings=38, top_n=2) # 14.07
 
-# looking for another 14.07 G/h layout:
-#best_power_layout_from_image('images/van16.png', n_total_buildings=38, top_n=1)
-#best_power_layout_from_image('images/van15.png', n_total_buildings=38, top_n=1)
-#best_power_layout_from_image('images/van14.png', n_total_buildings=38, top_n=1)
-#best_power_layout_from_image('images/van13.png', n_total_buildings=38, top_n=1)
-#best_power_layout_from_image('images/van12.png', n_total_buildings=38, top_n=1)
-#best_power_layout_from_image('images/van11.png', n_total_buildings=38, top_n=1)
-#best_power_layout_from_image('images/van10.png', n_total_buildings=38, top_n=1)
+    # looking for another 14.07 G/h layout:
+    #best_power_layout_from_image('images/van16.png', n_total_buildings=38, top_n=1)
+    #best_power_layout_from_image('images/van15.png', n_total_buildings=38, top_n=1)
+    #best_power_layout_from_image('images/van14.png', n_total_buildings=38, top_n=1)
+    #best_power_layout_from_image('images/van13.png', n_total_buildings=38, top_n=1)
+    #best_power_layout_from_image('images/van12.png', n_total_buildings=38, top_n=1)
+    #best_power_layout_from_image('images/van11.png', n_total_buildings=38, top_n=1)
+    #best_power_layout_from_image('images/van10.png', n_total_buildings=38, top_n=1)
 
-# run a search on a screenshot from the BaseScanner:
-best_power_layout_from_image('sample_layouts.png', n_total_buildings=38, top_n=2, next_level_threshold=13700000000)
-#best_power_layout_from_image('sample_layouts.png', n_total_buildings=38, top_n=1)
+    # run a search on a screenshot from the BaseScanner:
+    best_power_layout_from_image('sample_layouts.png', n_total_buildings=38, top_n=2, next_level_threshold=13700000000)
+    #best_power_layout_from_image('sample_layouts.png', n_total_buildings=38, top_n=1)
 
-# optimize some layouts given by a cnctaopt-link:
-#best_power_layout_from_url("https://cnctaopt.com/FDjPl", n_total_buildings=38, top_n=1) # 0.09s, 14.117 G/h
+    # optimize some layouts given by a cnctaopt-link:
+    #best_power_layout_from_url("https://cnctaopt.com/FDjPl", n_total_buildings=38, top_n=1) # 0.09s, 14.117 G/h
 
-# example where top_n=1 and top_n=2 return quite different results:
-#best_power_layout_from_url("https://cnctaopt.com/UsO7C", n_total_buildings=38, top_n=1) # 0.15s
-#best_power_layout_from_url("https://cnctaopt.com/UsO7C", n_total_buildings=38, top_n=2) # 33s
+    # example where top_n=1 and top_n=2 return quite different results:
+    #best_power_layout_from_url("https://cnctaopt.com/UsO7C", n_total_buildings=38, top_n=1) # 0.15s
+    #best_power_layout_from_url("https://cnctaopt.com/UsO7C", n_total_buildings=38, top_n=2) # 33s
 
-#best_power_layout_from_url("https://cnctaopt.com/XD7Uk", n_total_buildings=38, top_n=1) # 0.63s, 13.956 G/h
-#best_power_layout_from_url("https://cnctaopt.com/XD7Uk", n_total_buildings=38, top_n=2) # 35s, 14.020 G/h , [696:673]
+    #best_power_layout_from_url("https://cnctaopt.com/XD7Uk", n_total_buildings=38, top_n=1) # 0.63s, 13.956 G/h
+    #best_power_layout_from_url("https://cnctaopt.com/XD7Uk", n_total_buildings=38, top_n=2) # 35s, 14.020 G/h , [696:673]
